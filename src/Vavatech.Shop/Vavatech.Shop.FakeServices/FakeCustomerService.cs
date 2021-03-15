543741760b6ea8318bdb1e16d184eeb08541765a
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-15 14:30:18 +0100

    Utworzenie udawanej implementacji

---
+++ src/Vavatech.Shop/Vavatech.Shop.FakeServices/FakeCustomerService.cs
@@ -0,0 +1,52 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Vavatech.Shop.Fakers;
+using Vavatech.Shop.IServices;
+using Vavatech.Shop.Models;
+
+namespace Vavatech.Shop.FakeServices
+{
+    public class FakeCustomerService : ICustomerService
+    {
+        private readonly ICollection<Customer> customers;
+
+        // TODO: refactor
+        public FakeCustomerService()
+            : this(new CustomerFaker())
+        {
+
+        }
+
+        public FakeCustomerService(Faker<Customer> faker)
+        {
+            customers = faker.Generate(100);
+        }
+
+        public void Add(Customer entity)
+        {
+            customers.Add(entity);
+        }
+
+        public IEnumerable<Customer> Get()
+        {
+            return customers;
+        }
+
+        public Customer Get(int id)
+        {
+            return customers.SingleOrDefault(c => c.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            customers.Remove(Get(id));
+        }
+
+        public void Update(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
