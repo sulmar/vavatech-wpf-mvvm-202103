536a1fcbeb1df38f0f841ca415bc5539c63bb37a
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-15 09:39:24 +0100

    Utworzenie projektu

---
+++ src/Vavatech.Shop/Vavatech.Shop.WpfClient/01_XAML/XamlView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Vavatech.Shop.WpfClient._01_XAML
+{
+    /// <summary>
+    /// Interaction logic for XamlView.xaml
+    /// </summary>
+    public partial class XamlView : Window
+    {
+        public XamlView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Vavatech.Shop/Vavatech.Shop.WpfClient/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vavatech.Shop.WpfClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            //Window window1 = new Window();
+            //window1.Title = "XAML";
+
+            //Grid grid1 = new Grid();
+
+            //Button button1 = new Button();
+            //button1.Content = "Hello World!";
+            //grid1.Children.Add(button1);
+
+            //window1.Content = grid1;
+
+            // Windows Workflow Foundation (WWF)
+
+        }
+    }
+}
