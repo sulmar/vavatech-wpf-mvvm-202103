55d33d0d1fa0d473164fe8d462464998e32ac0ac
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-15 15:16:31 +0100

    Utworzenie ViewModels

---
+++ src/Vavatech.Shop/Vavatech.Shop.ViewModels/CustomersViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Vavatech.Shop.FakeServices;
+using Vavatech.Shop.IServices;
+using Vavatech.Shop.Models;
+
+namespace Vavatech.Shop.ViewModels
+{
+
+    public class CustomersViewModel : BaseViewModel
+    {
+        public IEnumerable<Customer> Customers { get; set; }
+
+        public Customer SelectedCustomer { get; set; }
+
+        private readonly ICustomerService customerService;
+
+        // TODO: refactor
+        public CustomersViewModel()
+            : this(new FakeCustomerService())
+        {
+
+        }
+
+        public CustomersViewModel(ICustomerService customerService)
+        {
+            this.customerService = customerService;
+
+            Load();
+        }
+
+        private void Load()
+        {
+            Customers = customerService.Get();
+        }
+    }
+}
