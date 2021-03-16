1e52b4b0a6d394355c9547e0b7548333d4879655
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-16 10:06:18 +0100

    Wstrzykiwanie zależności

--- src/Vavatech.Shop/Vavatech.Shop.WpfClient/ViewModelLocator.cs
+++ src/Vavatech.Shop/Vavatech.Shop.WpfClient/ViewModelLocator.cs
@@ -1,21 +1,38 @@
-﻿using System;
+﻿using Autofac;
+using Bogus;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Vavatech.Shop.Fakers;
 using Vavatech.Shop.FakeServices;
+using Vavatech.Shop.IServices;
+using Vavatech.Shop.Models;
 using Vavatech.Shop.ViewModels;
 
 namespace Vavatech.Shop.WpfClient
 {
+
+    // Install-Package AutoFac
+
     public class ViewModelLocator
     {
+        private readonly IContainer container;
+
         public ViewModelLocator()
         {
+            ContainerBuilder containerBuilder = new ContainerBuilder();
 
+            containerBuilder.RegisterType<CustomersViewModel>();
+            containerBuilder.RegisterType<FakeCustomerService>().As<ICustomerService>().SingleInstance();
+            containerBuilder.RegisterType<CustomerFaker>().As<Faker<Customer>>().SingleInstance();
+
+            container = containerBuilder.Build();
         }
 
-        public CustomersViewModel CustomersViewModel => new CustomersViewModel(new FakeCustomerService(new CustomerFaker()));
+        // public CustomersViewModel CustomersViewModel => new CustomersViewModel(new FakeCustomerService(new CustomerFaker()));
+
+        public CustomersViewModel CustomersViewModel => container.Resolve<CustomersViewModel>();
     }
 }
