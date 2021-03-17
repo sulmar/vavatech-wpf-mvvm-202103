27e15eafe92cb15c3d6c480139c481b58e27766a
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-17 12:24:04 +0100

    ValueConverter

---
+++ src/Vavatech.Shop/Vavatech.Shop.WpfClient/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Vavatech.Shop.WpfClient.Converters
+{
+
+    // wbudowany
+    // BooleanToVisibilityConverter
+    // https://docs.microsoft.com/en-us/dotnet/api/system.windows.controls.booleantovisibilityconverter?view=net-5.0
+
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool bValue = (bool)value;
+
+            if (targetType != typeof(Visibility))
+            {
+                throw new InvalidCastException("Oczekiwano Visilibity");
+            }
+
+            if (bValue)
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ src/Vavatech.Shop/Vavatech.Shop.WpfClient/Converters/InvertConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Vavatech.Shop.WpfClient.Converters
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class NegativeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+            {
+                throw new InvalidCastException("Oczekiwano boolean");
+            }
+
+            return !(bool)value;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
