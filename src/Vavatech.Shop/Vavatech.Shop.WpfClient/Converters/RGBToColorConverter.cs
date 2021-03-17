2a5d948f5b8003c09b11cb5bf94b56ac0a77d4c8
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-17 12:41:26 +0100

    MutliValueConverter

---
+++ src/Vavatech.Shop/Vavatech.Shop.WpfClient/Converters/RGBToColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Vavatech.Shop.WpfClient.Converters
+{
+    // [ValueConversion(typeof(IEnumerable<byte>), typeof(Color))]
+    public class RGBToColorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Color.FromRgb(
+                   r: System.Convert.ToByte(values[0]),
+                   g: System.Convert.ToByte(values[1]),
+                   b: System.Convert.ToByte(values[2])
+                );
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
