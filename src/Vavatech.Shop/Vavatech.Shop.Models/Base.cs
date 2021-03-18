c308c2761b007e9b9b31473c84403316fc3b2621
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-18 11:15:33 +0100

    Fix

--- src/Vavatech.Shop/Vavatech.Shop.Models/Base.cs
+++ src/Vavatech.Shop/Vavatech.Shop.Models/Base.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Specialized;
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
@@ -6,13 +8,32 @@ namespace Vavatech.Shop.Models
 {
     public abstract class Base : INotifyPropertyChanged
     {
+        #region INotifyPropertyChanged
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        #endregion
+
+        #region INotifyDataErrorInfo
+
+        //public bool HasErrors => throw new NotImplementedException();
+       
+        //public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        //public IEnumerable GetErrors(string propertyName)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        #endregion
+
+
     }
 
-  
+
 }
--- src/Vavatech.Shop/Vavatech.Shop.Models/Product.cs
+++ src/Vavatech.Shop/Vavatech.Shop.Models/Product.cs
@@ -1,41 +1,80 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Vavatech.Shop.Models
 {
     // Exceptions -> ValidatesOnExceptions=True
+
+    // INotifyDataErrorInfo -> ValidatesOnNotifyDataErrors = true
+
     public class Product : BaseEntity
     {
-        public string Name
-        {
-            get => name; 
-            set
-            {
-                if (string.IsNullOrEmpty(value))
-                    throw new ArgumentNullException();
+        public string Name { get; set; }
 
-                name = value;
-            }
-        }
+        //public string Name
+        //{
+        //    get => name; 
+        //    set
+        //    {
+        //        if (string.IsNullOrEmpty(value))
+        //            throw new ArgumentNullException();
+
+        //        name = value;
+        //    }
+        //}
+
         public string Color { get; set; }
-        public decimal UnitPrice
-        {
-            get => unitPrice; 
-            set
-            {
-                if (value <= 0 || value > 1000)
-                    throw new ArgumentOutOfRangeException();
 
-                unitPrice = value;
-            }
-        }
+        //public decimal UnitPrice
+        //{
+        //    get => unitPrice; 
+        //    set
+        //    {
+        //        if (value <= 0 || value > 1000)
+        //            throw new ArgumentOutOfRangeException();
+
+        //        unitPrice = value;
+        //    }
+        //}
+
+        public decimal UnitPrice { get; set; }
         public bool IsDiscount { get; set; }
         public decimal Discount { get; set; }
         public string ImageUrl { get; set; }
 
         public bool IsOverLimit => UnitPrice > UnitPriceOverLimit;
 
+
+
         public static decimal UnitPriceOverLimit = 500;
         private decimal unitPrice;
-        private string name;
+
+
+        //public IEnumerable GetErrors(string propertyName)
+        //{
+          
+        //    if (propertyName == nameof(Name))
+        //    {
+        //        if (string.IsNullOrEmpty(Name))
+        //        {
+        //            errors.Add("Nazwa nie może być pusta");
+        //        }
+        //    }
+
+        //    if (propertyName == nameof(UnitPrice))
+        //    {
+        //        if (UnitPrice <= 0 || UnitPrice > 1000)
+        //        {
+        //            errors.Add("Wartość UnitPrice jest spoza zakresu");
+        //        }
+        //    }
+
+         
+        //    return errors;
+        //}
+
+        
     }
 }
