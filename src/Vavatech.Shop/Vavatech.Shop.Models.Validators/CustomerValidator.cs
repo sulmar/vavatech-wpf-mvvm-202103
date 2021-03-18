d4009d06aecfe0a6c4a1a8a954e8cdbe9249f66d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-18 12:11:35 +0100

    Fluent Validation

---
+++ src/Vavatech.Shop/Vavatech.Shop.Models.Validators/CustomerValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Vavatech.Shop.Models.Validators
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            // hint: RuleSet
+            RuleFor(p => p.Land).NotEmpty().When(p => p.Country == Country.Germany);
+            RuleFor(p => p.TaxId).NotEmpty().When(p => p.Country == Country.Germany);
+
+            RuleFor(p => p.Pesel).NotEmpty().When(p => p.Country == Country.Poland);
+            RuleFor(p => p.Regon).NotEmpty().When(p => p.Country == Country.Poland);
+
+            RuleFor(p => p.TaxId).NotEmpty().When(p => p.Country == Country.Italy);
+
+        }
+    }
+}
---
+++ src/Vavatech.Shop/Vavatech.Shop.Models.Validators/ProductValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+
+namespace Vavatech.Shop.Models.Validators
+{
+
+    // Install-Package FluentValidation
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty();
+            RuleFor(p => p.UnitPrice).InclusiveBetween(1, 1000);
+            RuleFor(p => p.Discount).GreaterThan(0).When(p => p.IsDiscount);
+        }
+    }
+}
