a7b9ecef7877f84e36605ad2898b16d1ab55c927
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-18 10:36:04 +0100

    Walidacja z użyciem Exceptions

--- src/Vavatech.Shop/Vavatech.Shop.Models/Product.cs
+++ src/Vavatech.Shop/Vavatech.Shop.Models/Product.cs
@@ -1,10 +1,33 @@
-﻿namespace Vavatech.Shop.Models
+﻿using System;
+
+namespace Vavatech.Shop.Models
 {
+    // Exceptions -> ValidatesOnExceptions=True
     public class Product : BaseEntity
     {
-        public string Name { get; set; }
+        public string Name
+        {
+            get => name; 
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException();
+
+                name = value;
+            }
+        }
         public string Color { get; set; }
-        public decimal UnitPrice { get; set; }
+        public decimal UnitPrice
+        {
+            get => unitPrice; 
+            set
+            {
+                if (value <= 0 || value > 1000)
+                    throw new ArgumentOutOfRangeException();
+
+                unitPrice = value;
+            }
+        }
         public bool IsDiscount { get; set; }
         public decimal Discount { get; set; }
         public string ImageUrl { get; set; }
@@ -12,5 +35,7 @@
         public bool IsOverLimit => UnitPrice > UnitPriceOverLimit;
 
         public static decimal UnitPriceOverLimit = 500;
+        private decimal unitPrice;
+        private string name;
     }
 }
