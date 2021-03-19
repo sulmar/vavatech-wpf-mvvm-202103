7cefeb60fd04aaedd5989d004f2779db65737673
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-19 14:47:18 +0100

    Custom Control

---
+++ src/Vavatech.Shop/Vavatech.Shop.WpfClient/CustomControls/MyControl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace Vavatech.Shop.WpfClient.CustomControls
+{
+    [ContentProperty("MainContent")]
+    public class MyControl : Control
+    {
+        public double MyAngle
+        {
+            get { return (double)GetValue(MyAngleProperty); }
+            set { SetValue(MyAngleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Angle.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyAngleProperty =
+            DependencyProperty.Register("MyAngle", typeof(double), typeof(MyControl), new PropertyMetadata(0d));
+
+
+       
+        public object MainContent
+        {
+            get { return (object)GetValue(MainContentProperty); }
+            set { SetValue(MainContentProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MainContent.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MainContentProperty =
+            DependencyProperty.Register("MainContent", typeof(object), typeof(MyControl), new PropertyMetadata(null));
+
+
+        public MyControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyControl), new FrameworkPropertyMetadata(typeof(MyControl)));
+        }
+
+    }
+}
