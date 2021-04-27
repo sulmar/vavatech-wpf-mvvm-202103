d0a180bd6f33ae78fd6cae0ba840f70bf99b6b79
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-04-27 15:35:48 +0200

    Walidator numeryczny

---
+++ src/Vavatech.Shop/MetalDesignWpfClient/ValidationRules/NumericRangeRule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MetalDesignWpfClient.ValidationRules
+{
+    public class NumericRangeValidationRule : ValidationRule
+    {
+        public int? Minimum { get; set; }
+        public int? Maximum { get; set; }
+
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+
+            if (int.TryParse(value.ToString(), out int number))
+            {
+
+                if ( (Minimum.HasValue && number < Minimum) || (Maximum.HasValue && number > Maximum))
+                {
+                    return new ValidationResult(false, "Liczba poza zakresem");
+                }
+
+                return ValidationResult.ValidResult;
+
+            }
+            else
+            {
+                return new ValidationResult(false, "Liczba w złym formacie");
+            }
+        }
+    }
+}
---
+++ src/Vavatech.Shop/MetalDesignWpfClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetalDesignWpfClient.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public int Age { get; set; }
+    }
+}
