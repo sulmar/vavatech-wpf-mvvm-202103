fb4fe2b0db3122981dfc3a1805966be9485476bd
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-04-27 12:04:37 +0200

    ItemTemplateSelector

---
+++ src/Vavatech.Shop/Vavatech.Shop.WpfClient/DataTemplateSelectors/ItemTemplateSelector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Vavatech.Shop.Models;
+
+namespace Vavatech.Shop.WpfClient.DataTemplateSelectors
+{
+    public class ItemTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate ProductDataTemplate { get; set; }
+        public DataTemplate ServiceDataTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is Product)
+            {
+                return ProductDataTemplate;
+            }
+
+            if (item is Service)
+            {
+                return ServiceDataTemplate;
+            }
+
+            throw new NotSupportedException();
+        }
+    }
+}
