fd4d6e96815795e1763efb5921546ad3c7f64b41
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-04-28 17:04:00 +0200

    Drag and Drop

--- src/Vavatech.Shop/Vavatech.Shop.WpfClient/Converters/DragEventArgsConverter.cs
+++ src/Vavatech.Shop/Vavatech.Shop.WpfClient/Converters/DragEventArgsConverter.cs
@@ -6,12 +6,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
+using System.Windows.Markup;
 using Vavatech.Shop.Models;
 
 namespace Vavatech.Shop.WpfClient.Converters
 {
-    public class DragEventArgsConverter : IValueConverter
+    public class DragEventArgsConverter : MarkupExtension, IValueConverter
     {
+        private DragEventArgsConverter instance;
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             DragEventArgs e = (DragEventArgs)value;
@@ -24,6 +27,16 @@ namespace Vavatech.Shop.WpfClient.Converters
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+
+            return instance;
         }
     }
 }
