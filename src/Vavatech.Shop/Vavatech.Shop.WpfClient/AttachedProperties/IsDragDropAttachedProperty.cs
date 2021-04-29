98f52041070d13da4e657d80aca070b68fb47d63
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-04-29 10:26:27 +0200

    Porównanie

---
+++ src/Vavatech.Shop/Vavatech.Shop.WpfClient/AttachedProperties/IsDragDropAttachedProperty.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace Vavatech.Shop.WpfClient.AttachedProperties
+{
+    public class IsDragDropAttachedProperty
+    {
+        public static readonly DependencyProperty IsDragDropProperty =
+            DependencyProperty.RegisterAttached("IsDragDrop",
+                typeof(bool), typeof(Selector), new PropertyMetadata(false, IsDragDropPropertyChanged));
+
+        private static void IsDragDropPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var selector = (Selector)d;
+
+            if ((bool)e.NewValue)
+            {
+                selector.MouseMove += Selector_MouseMove;
+            }
+            else
+            {
+                selector.MouseMove -= Selector_MouseMove;
+            }
+        }
+
+        private static void Selector_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Selector selector = sender as Selector;
+
+                DragDrop.DoDragDrop(selector, selector.SelectedItem, DragDropEffects.Move);
+            }
+        }
+
+        public static bool GetIsDragDrop(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsDragDropProperty);
+        }
+
+        public static void SetIsDragDrop(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsDragDropProperty, value);
+        }
+    }
+}
---
+++ src/Vavatech.Shop/Vavatech.Shop.WpfClient/Behaviors/MouseMoveBehavior.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace Vavatech.Shop.WpfClient.Behaviors
+{
+    public class MouseMoveBehavior : Behavior<Selector>
+    {
+        protected override void OnAttached()
+        {
+            this.AssociatedObject.MouseMove += AssociatedObject_MouseMove;
+        }
+
+        private void AssociatedObject_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Selector selector = sender as Selector;
+
+                DragDrop.DoDragDrop(selector, selector.SelectedItem, DragDropEffects.Move);
+            }
+        }
+    }
+}
